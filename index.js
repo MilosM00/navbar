9fa44d31292d75bb024743da4ea600a1b303c350
unknown <milos.milijasevic00@gmail.com>
2023-04-15 20:04:13 +0200

    index-js updated

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const hamburgerButton = document.querySelector("#hamburger-button");
 const hamburgerUl = document.querySelector(".hamburger-ul");
+const hamburgerLi = document.querySelectorAll(".hamburger-li");
+const navImage = document.querySelector(".nav-image");
 let hamburger = true;
 
 const hamburgerTrue = () =>{
     hamburgerButton.classList.remove("fa-bars");
     hamburgerButton.classList.add("fa-times");
     hamburgerUl.style.display = "flex";
+    setTimeout(() => { hamburgerUl.style.opacity = "1"; }, 10);
     hamburger = false;
 };
 
 const hamburgerFalse = () =>{
     hamburgerButton.classList.add("fa-bars");
     hamburgerButton.classList.remove("fa-times");
-    hamburgerUl.style.display = "none";
+    setTimeout(() => { hamburgerUl.style.display = "none"; }, 500);
+    hamburgerUl.style.opacity = "0";
     hamburger = true;
 };
 
@@ -20,3 +24,12 @@ hamburgerButton.addEventListener("click", () =>{
     if(hamburger === true) hamburgerTrue();
     else hamburgerFalse();
 });
+
+hamburgerLi[0].addEventListener("click", () =>{ hamburgerFalse(); });
+hamburgerLi[1].addEventListener("click", () =>{ hamburgerFalse(); });
+hamburgerLi[2].addEventListener("click", () =>{ hamburgerFalse(); });
+hamburgerLi[3].addEventListener("click", () =>{ hamburgerFalse(); });
+hamburgerLi[4].addEventListener("click", () =>{ hamburgerFalse(); });
+hamburgerLi[5].addEventListener("click", () =>{ hamburgerFalse(); });
+hamburgerLi[6].addEventListener("click", () =>{ hamburgerFalse(); });
+navImage.addEventListener("click", () =>{ hamburgerFalse(); });
